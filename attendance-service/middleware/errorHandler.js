7a0f471e81ee89b0c01099b3f8af4e2cc54e6fb6
Chandana H <chandanaharish07@gmail.com>
2026-04-28 14:16:14 +0530

    cleanup: removing obsolete legacy files to simplify repo

--- attendance-service/middleware/errorHandler.js
+++
@@ -1,4 +0,0 @@
-module.exports = (err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ message: 'Internal Server Error', error: process.env.NODE_ENV === 'development' ? err.message : {} });
-};
